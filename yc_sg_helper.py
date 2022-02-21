6b91d8dcd256e8c75ff2d11f84f138d6763e8a2d
andrey.bobokhin <ab@devim.team>
2022-02-21 22:57:27 +0300

    first commit

---
+++ yc_sg_helper.py
@@ -0,0 +1,38 @@
+import pulumi
+import pulumi_yandex as yandex
+
+def allow_egress(sg_id: str):
+    yandex.VpcSecurityGroupRule("egress",
+        direction="egress",
+        v4_cidr_blocks=["0.0.0.0/0"],
+        port=-1,
+        protocol="ANY",
+        security_group_binding=sg_id
+    )
+
+def allow_ingress_self_sg(sg_id: str):
+    yandex.VpcSecurityGroupRule("ingress",
+        direction="ingress",
+        predefined_target="self_security_group",
+        port=-1,
+        protocol="TCP",
+        security_group_binding=sg_id
+    )
+
+def allow_ssh(sg_id: str):
+    yandex.VpcSecurityGroupRule("ssh",
+        direction="ingress",
+        v4_cidr_blocks=["0.0.0.0/0"],
+        port=22,
+        protocol="TCP",
+        security_group_binding=sg_id
+    )
+
+def allow_ssh_self_sg(sg_id: str):
+    yandex.VpcSecurityGroupRule("ssh",
+        direction="ingress",
+        predefined_target="self_security_group",
+        port=22,
+        protocol="TCP",
+        security_group_binding=sg_id
+    )
